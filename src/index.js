6d9229c8948877810e00bcd7bd574d7cfe4f4045
KIMSUHEON-1122 <59812809+KIMSUHEON-1122@users.noreply.github.com>
2022-11-16 12:20:36 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
-import AppProducts from './AppProducts';
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
